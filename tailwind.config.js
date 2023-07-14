dc2d93bf48a9f02155c67b9bf85c224dbfbfdf09
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2023-07-14 14:16:13 +0545

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  fontFamily:{
+    'ggsans': ['ggsans-normal'],
+        
+  },
+  spacing: {
+    '30' : '120px'
+  },
+}
+
+
+
+  
+
+
